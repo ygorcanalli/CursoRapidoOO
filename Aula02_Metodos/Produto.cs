d7c79b5c596d7e37e44bf9dea7d2742b7f137582
Ygor Canalli <ygor.canalli@gmail.com>
2021-06-19 12:00:51 -0300

    aula 2

--- Aula02_Metodos/Produto.cs
+++ Aula02_Metodos/Produto.cs
@@ -1,7 +1,22 @@
-﻿namespace Aula02_Metodos
+﻿using System;
+
+namespace Aula02_Metodos
 {
     public class Produto
     {
-        
+        public string Nome;
+        public double Preco;
+
+        public Produto(string nome, double preco)
+        {
+            this.Nome = nome;
+            this.Preco = preco;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("Nome: {0}", this.Nome);
+            Console.WriteLine("Preço: {0:0.00}\n", this.Preco);
+        }
     }
 }
---
+++ Aula02_Metodos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula02_Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto1 = new Produto("Camisa T-shirt", 59.90);
+            produto1.Imprimir();
+
+            Produto produto2 = new Produto("Calça Jeans", 89.90);
+            produto2.Imprimir();
+        }
+    }
+}
